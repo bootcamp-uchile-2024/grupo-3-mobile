1a6a63ca94de87dcf4c59d3f6c049077f26bd5e3
Cesar Sandoval <cesarsandoval.dev@gmail.com>
2024-08-13 17:29:24 -0400

    chore: shoping car wireframe

--- app/src/main/java/com/cotiledon/mobilApp/ShoppingCartActivity.kt
+++ app/src/main/java/com/cotiledon/mobilApp/ShoppingCartActivity.kt
@@ -3,15 +3,14 @@ package com.cotiledon.mobilApp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-
-class SignInActivity : AppCompatActivity() {
+class ShoppingCartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signin)
-
-
+        setContentView(R.layout.activity_shopping_cart)
     }
 }
