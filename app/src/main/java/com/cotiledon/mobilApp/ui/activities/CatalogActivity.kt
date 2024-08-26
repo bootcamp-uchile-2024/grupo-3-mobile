10d16b6a577190e116d57d1eea03ed3f7ea07673
Vicente Labbé <vicholabbe@gmail.com>
2024-08-26 01:38:10 -0400

    chore: "Add Catalog Layout"

---
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/CatalogActivity.kt
@@ -0,0 +1,18 @@
+package com.cotiledon.mobilApp.ui.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cotiledon.mobilApp.R
+
+class CatalogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_catalog)
+
+    }
+}
