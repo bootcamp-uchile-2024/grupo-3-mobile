dfad64e49cf6ea83235727a4452cf1f66c39ee03
Vicente Labbé <vicholabbe@gmail.com>
2024-08-26 20:29:28 -0400

    chore: "Add Plant class"

---
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/Plant.kt
@@ -0,0 +1,13 @@
+package com.cotiledon.mobilApp.ui.activities
+
+class Plant(plantName: String, plantPrice: String, plantDesc: String, plantID: Int, plantStock: Int, plantCat: String, plantImage: Array<Int>)
+     {
+         var plantName: String = "";
+         var plantPrice: String = "";
+         var plantDesc: String = "";
+         var plantID: Int = 0;
+         var plantStock: Int = 0;
+         var plantCat: String = "";
+         var plantImage: Array<Int> = emptyArray();
+
+     }
