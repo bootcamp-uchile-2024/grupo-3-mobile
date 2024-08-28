c611871a88b54a49316b857ab6c6bafc82313838
Vicente Labbé <vicholabbe@gmail.com>
2024-08-27 23:19:45 -0400

    chore: "Definition of CatalogActivity data retrieval logic"

--- app/src/main/java/com/cotiledon/mobilApp/ui/activities/CatalogActivity.kt
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/CatalogActivity.kt
@@ -1,18 +1,40 @@
 package com.cotiledon.mobilApp.ui.activities
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cotiledon.mobilApp.R
+import org.json.JSONArray
+import org.json.JSONObject
 
+@Suppress("UNCHECKED_CAST")
 class CatalogActivity : AppCompatActivity() {
+
+    val Plantas: MutableList<Plant> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_catalog)
 
+
+
     }
+
+    val objetosPlantas: JSONArray = JSONArray(resources.openRawResource((R.raw.products)).bufferedReader().use{it.readLine()})
+
+    fun setUpPlants(){
+        for (i in 0 ..< objetosPlantas.length()){
+            val Planta: JSONObject = JSONObject(objetosPlantas.get(i).toString())
+            val instance = Plant(Planta.get("name").toString(),Planta.get("price").toString(),Planta.get("desc").toString(),Planta.get("id") as Int,Planta.get("stock") as Int,
+                Planta.get("cat").toString(),Planta.get("pics") as Array<String>)
+            Plantas.add(instance)
+        }
+    }
+
 }
--- app/src/main/java/com/cotiledon/mobilApp/ui/activities/Plant.kt
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/Plant.kt
@@ -1,6 +1,6 @@
 package com.cotiledon.mobilApp.ui.activities
 
-class Plant(plantName: String, plantPrice: String, plantDesc: String, plantID: Int, plantStock: Int, plantCat: String, plantImage: Array<Int>)
+class Plant(plantName: String, plantPrice: String, plantDesc: String, plantID: Int, plantStock: Int, plantCat: String, plantImage: Array<String>)
      {
          var plantName: String = "";
          var plantPrice: String = "";
@@ -8,6 +8,6 @@ class Plant(plantName: String, plantPrice: String, plantDesc: String, plantID: I
          var plantID: Int = 0;
          var plantStock: Int = 0;
          var plantCat: String = "";
-         var plantImage: Array<Int> = emptyArray();
+         var plantImage: Array<String> = emptyArray();
 
      }
