d071d2e7121c2fa585c33a95da1eb7e6bf6e5f36
Vicente Labbé <vicholabbe@gmail.com>
2024-10-21 19:11:42 -0300

    Chore: "created Data Class CartPlant and CartRecyclerViewAdapter"

---
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/CartPlant.kt
@@ -0,0 +1,6 @@
+package com.cotiledon.mobilApp.ui.activities
+
+data class CartPlant(val plantName: String,
+                     val plantPrice: String,
+                    val plantQuantity: String,
+                     val plantImage: Int)
