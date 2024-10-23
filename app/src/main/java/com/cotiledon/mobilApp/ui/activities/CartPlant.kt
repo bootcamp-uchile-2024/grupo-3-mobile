3593c84d5f61a5b60101cf042953b9e0b56a00f8
Vicente Labbé <vicholabbe@gmail.com>
2024-10-23 01:01:52 -0300

    Chore: "Added CartStorage class"

--- app/src/main/java/com/cotiledon/mobilApp/ui/activities/CartPlant.kt
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/CartPlant.kt
@@ -2,5 +2,7 @@ package com.cotiledon.mobilApp.ui.activities
 
 data class CartPlant(val plantName: String,
                      val plantPrice: String,
+                     val plantID : String,
+                     val plantStock: String,
                     val plantQuantity: String,
                      val plantImage: Int)
--- app/src/main/java/com/cotiledon/mobilApp/ui/activities/ShoppingCartActivity.kt
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/ShoppingCartActivity.kt
@@ -3,12 +3,32 @@ package com.cotiledon.mobilApp.ui.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cotiledon.mobilApp.R
+
+
+private lateinit var recyclerView: RecyclerView
+private lateinit var adaptador: PlantRecyclerViewAdapter
+private var cartPlants = mutableListOf<Plant>()
 
 class ShoppingCartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_cart)
+
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.shoppingCartRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+        adaptador = CartRecyclerViewAdapter(cartPlants)
+        recyclerView.adapter = adaptador
+
     }
+
+
+
 }
