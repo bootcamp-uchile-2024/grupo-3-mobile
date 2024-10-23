38548c3f52218ca82e4e2f191c7102c5db6dde4e
Vicente Labbé <vicholabbe@gmail.com>
2024-10-23 01:41:09 -0300

    Chore: "Added cart functionality to ProductActivity"

--- app/src/main/java/com/cotiledon/mobilApp/ui/activities/ProductActivity.kt
+++ app/src/main/java/com/cotiledon/mobilApp/ui/activities/ProductActivity.kt
@@ -2,6 +2,7 @@ package com.cotiledon.mobilApp.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.cotiledon.mobilApp.R
 
 class ProductActivity : AppCompatActivity() {
+    private lateinit var cartStorage: CartStorage
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
@@ -31,14 +33,43 @@ class ProductActivity : AppCompatActivity() {
                 val plantPrice = intent.getStringExtra("plantPrice")
                 val plantDesc = intent.getStringExtra("plantDesc")
                 val plantImage = intent.getIntExtra("plantImage", 0)
+                val plantID = intent.getStringExtra("plantID")
+                val plantStock = intent.getStringExtra("plantStock")
+                val cartPlant = CartPlant(plantName.toString(), plantPrice.toString(),
+                    plantID.toString(), plantStock.toString(), 1, plantImage)
+
+                //Boton para agregar al carrito y modificadores de cantidad
+                val addToCartButton: Button = findViewById(R.id.addToCartButton)
+                val increaseQuantityButton: Button = findViewById(R.id.productQuantityIncrease)
+                val decreaseQuantityButton: Button = findViewById(R.id.productQuantityDecrease)
+                val quantityDisplay: TextView = findViewById(R.id.productQuantityDisplay)
+
+                cartStorage = CartStorage(this)
+
+                //Definir el comportamiento de los botones
+                increaseQuantityButton.setOnClickListener {
+                    cartPlant.plantQuantity++
+                    quantityDisplay.text = cartPlant.plantQuantity.toString()
+                }
+
+                decreaseQuantityButton.setOnClickListener {
+                    if (cartPlant.plantQuantity > 1) {
+                        cartPlant.plantQuantity--
+                        quantityDisplay.text = cartPlant.plantQuantity.toString()
+                    }
+
+                    addToCartButton.setOnClickListener {
+                        cartStorage.saveProductToCart(cartPlant)
+                    }
 
                 //Definir los datos que se mostrarán en la actividad
                 imageView.setImageResource(plantImage)
                 nameView.text = plantName
                 descView.text = plantDesc
                 priceView.text = plantPrice
-
+                }
             }
+
             //Iniciado desde el home
             "HomeActivity" -> {
                 //Recibir los datos
@@ -53,14 +84,7 @@ class ProductActivity : AppCompatActivity() {
             }
         }
 
-        val productQuantity = findViewById<TextView>(R.id.productQuantityDisplay)
-        val productIncrease = findViewById<ImageView>(R.id.productQuantityIncrease)
-        val productDecrease = findViewById<ImageView>(R.id.productQuantityDecrease)
 
-
-        fun onProductIncrease(){
-
-        }
 
 
         //Ir a al vista de carrito
