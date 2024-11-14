4b20ad5027db12210c882d47141c4bd748c4f318
Vicente Labbé <vicholabbe@gmail.com>
2024-11-14 01:12:03 -0300

    Chore: "Created singleton OrderManager"

---
+++ app/src/main/java/com/cotiledon/mobilApp/ui/managers/OrderManager.kt
@@ -0,0 +1,9 @@
+package com.cotiledon.mobilApp.ui.managers
+
+import com.cotiledon.mobilApp.ui.dataClasses.PaymentDetails
+import com.cotiledon.mobilApp.ui.dataClasses.ShippingDetails
+
+object OrderManager {
+    var shippingDetails: ShippingDetails? = null
+    var paymentDetails: PaymentDetails? = null
+}
